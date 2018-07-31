88a96ab9c3812bd0c5469922f78cfd5373ab5f8a
Fermin <ferminsandoval950@hotmail.com>
2018-07-31 14:29:47 -0600

    Fixed parseing error when capturing a piece

--- File IO/Controllers/FileParsing.cs
+++ File IO/Controllers/FileParsing.cs
@@ -20,44 +20,51 @@ namespace File_IO.Controllers
                 switch (line.Length)
                 {
                     case 4:
-                        PlacePiece(line);
+                        Console.WriteLine(PlacePiece(line));
                         break;
                     case 5:
-                        MoveSinglePiece(line);
+                        Console.WriteLine(MoveSinglePiece(line));
+                        break;
+                    case 6:
+                        Console.WriteLine(CapturePiece(line));
                         break;
                     case 11:
-                        MoveTwoPieces(line);
+                        Console.WriteLine(MoveTwoPieces(line));
                         break;
                 }
-                
+
             }
         }
 
-        private void PlacePiece(string line)
+        private string CapturePiece(string line)
+        {
+            string start = line[0].ToString().ToUpper() + line[1];
+            string end = line[3].ToString().ToUpper() + line[4];
+
+            return $"Move {start} to {end} and caputred piece at {end}";
+        }
+
+        private string PlacePiece(string line)
         {
             if (pieces.ChessPiece.TryGetValue(line[0].ToString(), out string piece))
             {
                 if (pieces.PieceColor.TryGetValue(line[1].ToString(), out string color))
                 {
-                    Console.WriteLine($"Place {color} {piece} on {line[2]}{line[3]}");
+                    return $"Place {color} {piece} on {line[2]}{line[3]}";
                 }
             }
+            return "";
         }
 
-        private void MoveTwoPieces(string line)
+        private string MoveTwoPieces(string line)
         {
-            Console.WriteLine("----------------------------------");
-            MoveSinglePiece(line.Substring(0, 5));
-            MoveSinglePiece(line.Substring(4, 5));
-            Console.WriteLine("----------------------------------");
+            return $"Moving two pieces: {MoveSinglePiece(line.Substring(0, 5))} AND {MoveSinglePiece(line.Substring(6, 5))}";
         }
 
-        private void MoveSinglePiece(string line)
+        private string MoveSinglePiece(string line)
         {
-            Console.WriteLine($"Move {line[0].ToString().ToUpper()}{line[1]} to " +
-                $"{line[3].ToString().ToUpper()}{line[4]}" );
+            return $"Move {line[0].ToString().ToUpper()}{line[1]} to " +
+                $"{line[3].ToString().ToUpper()}{line[4]}";
         }
-
-        
     }
 }
