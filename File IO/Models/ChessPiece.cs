7037b54124e09ef388c4362640bd8a5ebe6e4ea4
Fermin <ferminsandoval950@hotmail.com>
2018-08-01 11:13:28 -0600

    Added ChessPiece class

---
+++ File IO/Models/ChessPiece.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO.Models
+{
+
+    public enum Pieces
+    {
+        K, Q, B, N, R, P,
+    }
+
+    public enum PieceColor
+    {
+        L, D
+    }
+    class ChessPiece
+    {
+        public Pieces Piece{ get; set; }
+
+        public PieceColor Color { get; set; }
+
+
+        public ChessPiece(Pieces piece, PieceColor color)
+        {
+            this.Piece = piece;
+            this.Color = color;
+        }
+
+        //Light is Uppercase
+        public override string ToString()
+        {
+            switch (this.Color)
+            {
+                case PieceColor.L:
+                    return $"{this.Piece.ToString()}";
+                case PieceColor.D:
+                    return $"{this.Piece.ToString().ToLower()}";
+            }
+
+            return "";
+        }
+    }
+}
