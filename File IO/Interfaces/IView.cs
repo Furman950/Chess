188992e0d6b1cd2f7d3aa3e0d6ecba103653838d
Brevlin Thompson <bthompson@student.neumont.edu>
2018-08-01 11:14:20 -0600

    Added View for console display

---
+++ File IO/Interfaces/IView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Interfaces
+{
+    interface IView
+    {
+        /// <summary>
+        /// Displays the current Chess Board State. The implementation must use null to show that there's no piece on a
+        /// specific board
+        /// </summary>
+        /// <param name="board">The board being displayed</param>
+        void DisplayBoard(Board board);
+
+        
+    }
+}
---
+++ File IO/View/ConsoleView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.Interfaces;
+
+namespace Chess.View
+{
+    class ConsoleView : IView
+    {
+        public void DisplayBoard(Board board) {
+            Console.WriteLine(board.ToString());
+        }
+    }
+}
