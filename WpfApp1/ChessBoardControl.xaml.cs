bbb6f159529ec8ed565147847762ada52055b6ee
Michael Zopff <michael.zopff@gmail.com>
2018-08-07 10:37:29 -0600

    Change ChessBoardControl's Namespace

--- WpfApp1/ChessBoardControl.xaml.cs
+++ WpfApp1/ChessBoardControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace ChessDisplay
 {
     /// <summary>
     /// Interaction logic for ChessBoardControl.xaml
