f2d99037ccff26f6f49cfab24aab13afbead8d9b
Michael Zopff <michael.zopff@gmail.com>
2018-08-15 11:12:38 -0600

    Move MovingPiece class functionality into Board class

--- WpfApp1/Models/Board.cs
+++ WpfApp1/Models/Board.cs
@@ -8,11 +8,12 @@ using WpfApp1.Controllers;
 namespace File_IO.Models {
     public class Board {
         private ChessPiece[][] board;
+        private PieceColor lastColor = PieceColor.D;
 
         public Board() {
             board = new ChessPiece[8][];
-            for (int i = 0; i < board.Length; i++) {
-                board[i] = new ChessPiece[8];
+            for (int y = 0; y < board.Length; y++) {
+                board[y] = new ChessPiece[8];
             }
         }
 
@@ -47,7 +48,7 @@ namespace File_IO.Models {
             for (int y = 0; y < board.Length; y++) {
                 for (int x = 0; x < board[y].Length; x++) {
                     if (board[y][x] != null && board[y][x].Color != kingColor) {
-                        if (WpfApp1.Controllers.MovingPiece.CheckMove(x, y, kingX, kingY, this)) {
+                        if (CheckMove(x, y, kingX, kingY)) {
                             return true;
                         }
                     }
@@ -79,11 +80,7 @@ namespace File_IO.Models {
                             for (int toY = 0; toY < board.Length; toY++) {
                                 for (int toX = 0; toX < board[toY].Length; toX++) {
                                     Board boardClone = this.Clone();
-                                    MovingPiece testMove = new MovingPiece();
-                                    if (kingColor == PieceColor.D) {
-                                        testMove.FlipColor();
-                                    }
-                                    if (testMove.Move(startX, startY, toX, toY, boardClone) &&
+                                    if (boardClone.Move(startX, startY, toX, toY) &&
                                         !boardClone.Check(kingColor)) {
                                         return false;
                                     }
@@ -99,13 +96,23 @@ namespace File_IO.Models {
         }
 
         public Board Clone() {
-            Board newBoard = new Board();
+            Board newBoard = new Board() {
+                lastColor = this.lastColor
+            };
             for (int y = 0; y < board.Length; y++) {
                 for (int x = 0; x < board[y].Length; x++) {
                     newBoard[x, y] = board[y][x];
                 }
             }
             return newBoard;
+        }
+        public void Copy(Board otherBoard) {
+            for (int y = 0; y < board.Length; y++) {
+                for (int x = 0; x < board[y].Length; x++) {
+                    board[y][x] = otherBoard[x, y];
+                }
+            }
+            lastColor = otherBoard.lastColor;
         }
 
         public override string ToString() {
@@ -135,6 +142,163 @@ namespace File_IO.Models {
                 }
             }
             return output.ToString();
+        }
+
+        public bool Move(int locationX, int locationY, int toX, int toY) {
+            ChessPiece movingPiece = this[locationX, locationY];
+            if (movingPiece != null) {
+                if (this.CheckMove(locationX, locationY, toX, toY) && movingPiece.Color != lastColor) {
+                    Board boardClone = this.Clone();
+                    boardClone[locationX, locationY] = null;
+                    boardClone[toX, toY] = movingPiece;
+                    if (!boardClone.Check(movingPiece.Color)) {
+                        this.Copy(boardClone);
+                        lastColor = movingPiece.Color;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private bool CheckMove(int locationX, int locationY, int toX, int toY) {
+            bool result = false;
+            switch (this[locationX, locationY].Piece) {
+                case Pieces.K:
+                    result = MoveKing(locationX, locationY, toX, toY);
+                    break;
+                case Pieces.Q:
+                    result = MoveQueen(locationX, locationY, toX, toY);
+                    break;
+                case Pieces.B:
+                    result = MoveBishop(locationX, locationY, toX, toY);
+                    break;
+                case Pieces.N:
+                    result = MoveKnight(locationX, locationY, toX, toY);
+                    break;
+                case Pieces.R:
+                    result = MoveRook(locationX, locationY, toX, toY);
+                    break;
+                case Pieces.P:
+                    result = MovePawn(locationX, locationY, toX, toY);
+                    break;
+            }
+            return result;
+        }
+
+        private bool MovePawn(int locationX, int locationY, int toX, int toY) {
+            ChessPiece movingPiece = this[locationX, locationY];
+            int colorCoefficient = 1;
+            if (movingPiece.Color == PieceColor.D) {
+                colorCoefficient = -1;
+            }
+            if (locationX == toX) {
+                //Two-space movement check
+                if ((locationY == 0 || locationY == 6) && toY - locationY == 2 * colorCoefficient) {
+                    if (this[locationX, locationY + colorCoefficient] == null &&
+                        this[locationX, locationY + colorCoefficient * 2] == null) {
+                        return true;
+                    }
+                } else if (toY - locationY == colorCoefficient) {           //One-space movement check
+                    if (this[locationX, locationY + colorCoefficient] == null) {
+                        return true;
+                    }
+                }
+            } else if (Math.Abs(toX - locationX) == 1 && toY - locationY == colorCoefficient) {     //Capture check
+                if (this[locationX, locationY + colorCoefficient] != null) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool MoveRook(int locationX, int locationY, int toX, int toY) {
+            if (locationX == toX ^ locationY == toY) {
+                return CheckDirection(locationX, locationY, toX, toY);
+            } else {
+                return false;
+            }
+        }
+
+        private bool MoveKnight(int locationX, int locationY, int toX, int toY) {
+            bool isValidLocation = IsValidMoveKnight(locationX, locationY, toX, toY);
+            ChessPiece placeMovedTo = this.GetPiece(toX, toY);
+            bool isNotOccupiedByFriendlyPiece = placeMovedTo == null ||
+                placeMovedTo.Color != this[locationX, locationY].Color;
+            bool isValidMove = isValidLocation && isNotOccupiedByFriendlyPiece;
+            return isValidMove;
+        }
+
+        private bool IsValidMoveKnight(int locationX, int locationY, int toX, int toY) {
+            bool isValid;
+            int absoluteValueX = Math.Abs(locationX - toX);
+            int absoluteValueY = Math.Abs(locationY - toY);
+            switch (absoluteValueX) {
+                case 1:
+                    isValid = absoluteValueY == 2;
+                    break;
+                case 2:
+                    isValid = absoluteValueY == 1;
+                    break;
+                default:
+                    isValid = false;
+                    break;
+            }
+            return isValid;
+        }
+
+        private bool MoveBishop(int locationX, int locationY, int toX, int toY) {
+            if (Math.Abs(locationX - toX) == Math.Abs(locationY - toY) && Math.Abs(locationX - toX) != 0) {
+                return CheckDirection(locationX, locationY, toX, toY);
+            } else {
+                return false;
+            }
+        }
+
+        private bool MoveQueen(int locationX, int locationY, int toX, int toY) {
+            if (locationX == toX && locationY == toY) {
+                return false;
+            } else {
+                return CheckDirection(locationX, locationY, toX, toY);
+            }
+        }
+
+        private bool MoveKing(int locationX, int locationY, int toX, int toY) {
+            if (Math.Abs(locationX - toX) < 2 && Math.Abs(locationY - toY) < 2 &&
+                !(locationX == toX && locationY == toY)) {
+                return this[toX, toY] == null || this[toX, toY].Color != this[locationX, locationY].Color;
+            } else {
+                return false;
+            }
+        }
+
+        private bool CheckDirection(int locationX, int locationY, int toX, int toY) {
+            return CheckDirection(locationX, locationY, toX, toY, this[locationX, locationY]);
+        }
+        private bool CheckDirection(int locationX, int locationY, int toX, int toY, ChessPiece startPiece) {
+            if (locationX == toX && locationY == toY) {
+                if (this[locationX, locationY] == null ||
+                    this[locationX, locationY].Color != startPiece.Color) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if ((locationX != toX && locationY != toY) ||
+                Math.Abs(locationX - toX) != Math.Abs(locationY - toY)) {
+                return false;
+            } else if (this[locationX, locationY] == null || this[locationX, locationY] == startPiece) {
+                if (locationX == toX) {
+                    return CheckDirection(locationX, locationY + (Math.Abs(toY - locationY) / (toY - locationY)),
+                        toX, toY, startPiece);
+                } else if (locationY == toY) {
+                    return CheckDirection(locationX + (Math.Abs(toX - locationX) / (toX - locationX)), locationY,
+                        toX, toY, startPiece);
+                } else {
+                    return CheckDirection(locationX + (Math.Abs(toX - locationX) / (toX - locationX)),
+                        locationY + (Math.Abs(toY - locationY) / (toY - locationY)), toX, toY, startPiece);
+                }
+            } else {
+                return false;
+            }
         }
     }
 }
