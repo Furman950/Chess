83e820aa68af471bc0a8c89e2df5addd31461b0d
Michael Zopff <michael.zopff@gmail.com>
2018-08-15 11:13:42 -0600

    Remove remaining references to Controllers

--- WpfApp1/Models/Board.cs
+++ WpfApp1/Models/Board.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WpfApp1.Controllers;
 
 namespace File_IO.Models {
     public class Board {
