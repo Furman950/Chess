4d80ce8e821475540e0e03aa9c77ff337af3e464
Fermin <ferminsandoval950@hotmail.com>
2018-08-28 10:04:50 -0600

    Added pawn promotion window

---
+++ WpfApp1/PawnPromotion.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ChessDisplay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for PawnPromotion.xaml
+    /// </summary>
+    public partial class PawnPromotion : Window
+    {
+        public Pieces ChosenPiece { get; set; }
+        public PawnPromotion()
+        {
+            InitializeComponent();
+            var pawnPromotion = this.Owner as ChessDisplay.MainWindow;
+            
+        }
+    }
+}
